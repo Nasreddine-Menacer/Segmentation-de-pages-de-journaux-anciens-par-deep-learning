bc481dd21ba264d074462290a33117d09ca19e53
Nasreddine Menacer <109802352+Nasreddine-Menacer@users.noreply.github.com>
2022-07-24 00:48:27 +0200

    Add files via upload

---
+++ prediction.py
@@ -0,0 +1,172 @@
+﻿dir_data = "dataset2/"
+dir_seg = dir_data + "/annotations_prepped_train/"
+dir_img = dir_data + "/images_prepped_train/"
+
+import cv2, os
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+## seaborn has white grid by default so I will get rid of this.
+sns.set_style("whitegrid", {'axes.grid' : False})
+
+
+ldseg = np.array(os.listdir(dir_seg))
+## pick the first image file
+fnm = ldseg[0]
+print(fnm)
+
+## read in the original image and segmentation labels
+seg = cv2.imread(dir_seg + fnm ) # (360, 480, 3)
+img_is = cv2.imread(dir_img + fnm )
+print("seg.shape={}, img_is.shape={}".format(seg.shape,img_is.shape))
+
+## Check the number of labels
+
+n_classes = 3
+
+
+import random
+def give_color_to_seg_img(seg,n_classes):
+    '''
+    seg : (input_width,input_height,3)
+    '''
+    
+    if len(seg.shape)==3:
+        seg = seg[:,:,0]
+    seg_img = np.zeros( (seg.shape[0],seg.shape[1],3) ).astype('float')
+    colors = sns.color_palette("hls", n_classes)
+    
+    for c in range(n_classes):
+        segc = (seg == c)
+        seg_img[:,:,0] += (segc*( colors[c][0] ))
+        seg_img[:,:,1] += (segc*( colors[c][1] ))
+        seg_img[:,:,2] += (segc*( colors[c][2] ))
+
+    return(seg_img)
+
+input_height , input_width = 224 , 224
+output_height , output_width = 224 , 224
+
+
+ldseg = np.array(os.listdir(dir_seg))
+for fnm in ldseg[np.random.choice(len(ldseg),3,replace=False)]:
+    fnm = fnm.split(".")[0]
+    seg = cv2.imread(dir_seg + fnm + ".png") # (360, 480, 3)
+    img_is = cv2.imread(dir_img + fnm + ".png")
+    seg_img = give_color_to_seg_img(seg,n_classes)
+
+
+
+
+def getImageArr( path , width , height ):
+        img = cv2.imread(path, 1)
+        img = np.float32(cv2.resize(img, ( width , height ))) / 127.5 - 1
+        return img
+
+def getSegmentationArr( path , nClasses ,  width , height  ):
+
+    seg_labels = np.zeros((  height , width  , nClasses ))
+    img = cv2.imread(path, 1)
+    img = cv2.resize(img, ( width , height ))
+    img = img[:, : , 0]
+
+    for c in range(nClasses):
+        seg_labels[: , : , c ] = (img == c ).astype(int)
+    ##seg_labels = np.reshape(seg_labels, ( width*height,nClasses  ))
+    return seg_labels
+
+
+
+
+images = os.listdir(dir_img)
+images.sort()
+segmentations  = os.listdir(dir_seg)
+segmentations.sort()
+    
+X = []
+Y = []
+for im , seg in zip(images,segmentations) :
+    X.append( getImageArr(dir_img + im , input_width , input_height )  )
+    Y.append( getSegmentationArr( dir_seg + seg , n_classes , output_width , output_height )  )
+
+X, Y = np.array(X) , np.array(Y)
+
+
+
+
+
+
+from sklearn.utils import shuffle
+train_rate = 0.85
+index_train = np.random.choice(X.shape[0],int(X.shape[0]*train_rate),replace=False)
+index_test  = list(set(range(X.shape[0])) - set(index_train))
+                            
+X, Y = shuffle(X,Y)
+X_train, y_train = X[index_train],Y[index_train]
+X_test, y_test = X[index_test],Y[index_test]
+
+
+from keras.models import load_model
+model = load_model('Mon_modele2.h5')
+
+model.compile(loss='binary_crossentropy',
+              optimizer='rmsprop',
+              metrics=['accuracy'])
+
+
+y_pred = model.predict(X_test)
+y_predi = np.argmax(y_pred, axis=3)
+y_testi = np.argmax(y_test, axis=3)
+
+
+
+
+
+def IoU(Yi,y_predi):
+    ## mean Intersection over Union
+    ## Mean IoU = TP/(FN + TP + FP)
+
+    IoUs = []
+    Nclass = int(np.max(Yi)) + 1
+    for c in range(Nclass):
+        TP = np.sum( (Yi == c)&(y_predi==c) )
+        FP = np.sum( (Yi != c)&(y_predi==c) )
+        FN = np.sum( (Yi == c)&(y_predi != c)) 
+        IoU = TP/float(TP + FP + FN)
+        print("class {:02.0f}: #TP={:6.0f}, #FP={:6.0f}, #FN={:5.0f}, IoU={:4.3f}".format(c,TP,FP,FN,IoU))
+        IoUs.append(IoU)
+    mIoU = np.mean(IoUs)
+    print("_________________")
+    print("Mean IoU: {:4.3f}".format(mIoU))
+    
+IoU(y_testi,y_predi)
+
+
+
+
+
+
+
+shape = (224,224)
+n_classes= 3
+
+for i in range(12):
+    img_is  = (X_test[i] + 1)*(255.0/2)
+    seg = y_predi[i]
+    segtest = y_testi[i]
+
+    fig = plt.figure(figsize=(10,30))    
+    ax = fig.add_subplot(1,3,1)
+    ax.imshow(img_is/255.0)
+    ax.set_title("original")
+    
+    ax = fig.add_subplot(1,3,2)
+    ax.imshow(give_color_to_seg_img(seg,n_classes))
+    ax.set_title("predicted class")
+    
+    ax = fig.add_subplot(1,3,3)
+    ax.imshow(Y[i])
+    ax.set_title("true class")
+    plt.show()
+    plt.savefig(str(i)+'.png')
+
